d87e8aa12575ffffc37cfea06bc53527bee22749
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2025-02-23 01:24:27 +0300

    feat: integrate Geist font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,15 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
